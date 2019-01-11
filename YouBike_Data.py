e8f98e1c8a371f53bc7b22841aa584d5310b87d1
yuchia0221 <46244904+yuchia0221@users.noreply.github.com>
2019-01-11 15:47:49 +0800

    Update YouBike_Data.py

--- YouBike_Data.py
+++ YouBike_Data.py
@@ -7,7 +7,7 @@ def datetimeConverter(datestring):
     return datetime.strptime(datestring, '%Y%m%d%H%M%S')
 
 # 讀取幾月幾號的YouBike站資料,並將其轉換成有效率和方便存取的資料結構
-def read_json(day=19,_month=10):
+def read_json(day=19, month=10):
     station_dict = defaultdict(deque)
     YouBike = namedtuple(
         'YouBike', 'TotalSpace BikeNum Time EmptySpace Delta')
